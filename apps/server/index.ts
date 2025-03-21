393ceb2faf4aab9ccc4ce13ad62267a2ae3f3aae
melosh101 <git@milasholsting.dk>
2025-03-21 09:07:59 +0100

    add base server api

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  return { hello: 'world' }
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
