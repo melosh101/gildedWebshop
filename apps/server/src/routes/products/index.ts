0fc5b23aa6ec18c40069dd961f48d646bfcd49bb
melosh101 <git@milasholsting.dk>
2025-03-24 13:32:19 +0100

    server: add get product by id

--- apps/server/src/routes/products/index.ts
+++ apps/server/src/routes/products/index.ts
@@ -1,17 +1,34 @@
 import { FastifyPluginAsync } from "fastify"
 import db from "../../db";
 import z from "zod";
-import { productTable } from "../../db/schema";
-import { sql } from "drizzle-orm";
+import { productTable, productVariantTable } from "../../db/schema";
+import { eq, sql } from "drizzle-orm";
 
 const productList = z.object({
   page: z.coerce.number().min(1).default(1),
   count: z.coerce.number().min(1).default(20)
 })
 
+const productParams = z.object({
+  id: z.coerce.number()
+})
+
 const products: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return { hello: 'world' }
+    const body = productParams.safeParse(request.query);
+
+    if(body.success === false) {
+      reply.status(400).send(body.error);
+      return;
+    }
+
+    const product = await db.query.productTable.findFirst({
+      where: eq(productTable.id, body.data.id),
+      with: {
+        productVariant: true
+      }
+    })
+    return product;
   })
 
   fastify.get('/list', async function (request, reply) {
